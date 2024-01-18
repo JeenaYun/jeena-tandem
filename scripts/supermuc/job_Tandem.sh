0a700b3f5685b918accf616e5c03c322ac8607a9
Jeena Yun <j4yun@ucsd.edu>
2024-01-17 16:49:17 -0800

    allow checkpointing feature

---
+++ scripts/supermuc/job_Tandem.sh
@@ -0,0 +1,12 @@
+model_n=BP1
+branch_n=reference
+setup_dir=$WORK/$model_n
+output_dir=$SCRATCH/$model_n/$branch_n/outputs
+export param_file=$setup_dir/bp1_sym.toml
+export ckp_f_step=50
+export ckp_f_ptime=1000000000
+export ckp_f_ctime=10
+n_nodes=10
+mkdir -p $output_dir
+
+sbatch -J $branch_n -o $setup_dir/messages_%x.log -e $setup_dir/messages_%x.err --chdir=$output_dir --nodes=$n_nodes run_Tandem_supermuc.sh
